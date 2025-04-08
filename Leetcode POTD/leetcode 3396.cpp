72ba6c7f62e776fdfc784f4a22e2ecf6e2c8bd60
devesh1808 <devesh1808@gmail.com>
2025-04-08 22:33:59 +0530

    brute force approach

---
+++ Leetcode POTD/leetcode 3396.cpp
@@ -0,0 +1,42 @@
+// Brute Force : O(n^2)
+
+bool isDistinct(unordered_map<int, int> mp)
+{
+    for (auto it : mp)
+    {
+        if (it.second > 1)
+            return false;
+    }
+    return true;
+}
+int minimumOperations(vector<int> &nums)
+{
+    int n = nums.size();
+    if (n == 1)
+        return 0;
+    if (n == 2)
+        return nums[0] != nums[1] ? 0 : 1;
+    int cnt = 0, i = 0, k = 0;
+    unordered_map<int, int> mp;
+    for (int ele : nums)
+    {
+        mp[ele]++;
+    }
+    if (isDistinct(mp))
+        return cnt;
+    while (i < n)
+    {
+        k += 3;
+        while (i < k)
+        {
+            if (i >= n)
+                break;
+            mp[nums[i]]--;
+            i++;
+        }
+        cnt++;
+        if (isDistinct(mp))
+            return cnt;
+    }
+    return cnt;
+}
